6d44a1936416e7e8ed0f7edac9acbf7fbe1f580d
petrepan <peterawotola@gmail.com>
2021-05-31 07:27:15 +0100

    made changes to the api responses

--- controllers/loan.js
+++ controllers/loan.js
@@ -4,137 +4,148 @@ const Loan = require("../models/Loan");
 const portfolios = require("../data/index");
 
 const getLoan = async (req, res) => {
- try {
-  const loanDetails = await Loan.findOne({ user: req.user._id });
-  const user = await User.findById(req.user._id);
-  user.totalPortfolioValue(portfolios);
+  try {
+    const loanDetails = await Loan.findOne({ user: req.user._id });
+    const user = await User.findById(req.user._id);
+    user.totalPortfolioValue(portfolios);
 
-  let portfolioValue = user.portfolioValue;
+    let portfolioValue = user.portfolioValue;
 
-  if (loanDetails.active) {
-   return res.status(200).send({
-    status: "success",
-    data: loanDetails,
-   });
-  } else {
-   if (portfolioValue >= 10000) {
-    return res.status(200).send({
-     status: "success",
-     message: "No Active Loan",
-     data: {
-      active: false,
-     },
-    });
-   } else {
-    return res.status(200).send({
-     status: "success",
-     message: "You Are Not Eligible for a Loan Now",
-     data: {
-      active: false,
-     },
-    });
-   }
+    if (loanDetails.active) {
+      return res.status(200).send({
+        status: "success",
+        data: {
+          user: loanDetails.user,
+          amount: loanDetails.amount,
+          balance: loanDetails.balance,
+          duration: loanDetails.duration,
+          active: loanDetails.active,
+          portfolioValue,
+          date: loanDetails.updatedAt,
+        },
+      });
+    } else {
+      if (portfolioValue >= 10000) {
+        return res.status(200).send({
+          status: "success",
+          message: "No Active Loan",
+          data: {
+            active: false,
+            portfolioValue,
+          },
+        });
+      } else {
+        return res.status(200).send({
+          status: "success",
+          message: "You Are Not Eligible for a Loan Now",
+          data: {
+            active: false,
+            portfolioValue,
+          },
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
   }
- } catch (error) {
-  console.log(error.message);
- }
 };
 
 const applyForLoan = async (req, res) => {
- const { amount, percentage, duration } = req.body;
+  const { amount, percentage, duration } = req.body;
 
- if (!amount || !percentage || !duration) {
-  return res.status(400).send({
-   message: "All fields are required",
-   status: "failed",
-   data: null,
-  });
- }
-
- try {
-  const loan = await Loan.findOne({ user: req.user._id });
-  const user = await User.findById(req.user._id);
-
-  user.totalPortfolioValue(portfolios);
-
-  let portfolioValue = user.portfolioValue;
-
-  if (loan.active === false) {
-   if (portfolioValue >= 10000) {
-    loan.amount = amount;
-    loan.balance = amount;
-    loan.percentage = percentage;
-    loan.duration = duration;
-    loan.active = true;
-
-    await loan.save();
-
-    return res.status(201).send({
-     status: "success",
-     message: "You Loan Has Been Activated",
+  if (!amount || !percentage || !duration) {
+    return res.status(400).send({
+      message: "All fields are required",
+      status: "failed",
+      data: null,
     });
-   } else {
-    return res.status(401).send({
-     status: "failed",
-     message: "You are not eligible to apply for Loan",
-    });
-   }
-  } else {
-   return res.status(401).send({
-    status: "failed",
-    message: "You have an Active Loan Already. Payback the loan to be eligible",
-   });
   }
- } catch (error) {}
+
+  try {
+    const loan = await Loan.findOne({ user: req.user._id });
+    const user = await User.findById(req.user._id);
+
+    user.totalPortfolioValue(portfolios);
+
+    let portfolioValue = user.portfolioValue;
+
+    if (loan.active === false) {
+      if (portfolioValue >= 10000) {
+        loan.amount = amount;
+        loan.balance = amount;
+        loan.percentage = percentage;
+        loan.duration = duration;
+        loan.active = true;
+
+        await loan.save();
+
+        return res.status(201).send({
+          status: "success",
+          message: "You Loan Has Been Activated",
+        });
+      } else {
+        return res.status(401).send({
+          status: "failed",
+          message: "You are not eligible to apply for Loan",
+        });
+      }
+    } else {
+      return res.status(401).send({
+        status: "failed",
+        message:
+          "You have an Active Loan Already. Payback the loan to be eligible",
+      });
+    }
+  } catch (error) {}
 };
 
 const payBackLoan = async (req, res) => {
- const { amount } = req.body;
+  const { amount } = req.body;
 
- if (!amount) {
-  return res.status(400).send({
-   message: "All fields are required",
-   status: "failed",
-   data: null,
-  });
- }
- try {
-  const loan = await Loan.findOne({ user: req.user._id });
-
-  if (loan.active) {
-   if (amount === loan.amount) {
-    loan.amount = 0;
-    loan.balance = 0;
-    loan.percentage = 0;
-    loan.duration = "None";
-    loan.active = false;
-
-    await loan.save();
-
-    return res.status(201).send({
-     status: "success",
-     message: `${amount} has been paid`,
+  if (!amount) {
+    return res.status(400).send({
+      message: "All fields are required",
+      status: "failed",
+      data: null,
     });
-   } else {
-    loan.amount = loan.amount;
-    loan.balance = loan.amount - Number(amount);
-    loan.percentage = loan.percentage;
-    loan.duration = loan.duration;
-    loan.active = true;
-
-    await loan.save();
-    return res.status(201).send({
-     status: "success",
-     message: `${amount} has been paid`,
-    });
-   }
-  } else {
-   return res.status(422).send({
-    status: "failed",
-    message: "You have no Active Loan.",
-   });
   }
- } catch (error) {}
+  try {
+    const loan = await Loan.findOne({ user: req.user._id });
+
+    if (loan.active) {
+      if (amount === loan.amount) {
+        loan.amount = 0;
+        loan.balance = 0;
+        loan.percentage = 0;
+        loan.duration = "None";
+        loan.active = false;
+
+        await loan.save();
+
+        return res.status(201).send({
+          status: "success",
+          message: `${amount} has been paid`,
+        });
+      } else {
+        loan.amount = loan.amount;
+        loan.balance = loan.amount - Number(amount);
+        loan.percentage = loan.percentage;
+        loan.duration = loan.duration;
+        loan.active = true;
+
+        await loan.save();
+        return res.status(201).send({
+          status: "success",
+          message: `${amount} has been paid`,
+        });
+      }
+    } else {
+      return res.status(422).send({
+        status: "failed",
+        message: "You have no Active Loan.",
+      });
+    }
+  } catch (error) {}
 };
 
 module.exports = { getLoan, applyForLoan, payBackLoan };
